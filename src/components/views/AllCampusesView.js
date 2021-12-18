364fa172f2ae3daf7db7cae5c81674a0f4351396
Jane <fjane1210@gmail.com>
2021-12-17 22:17:55 -0500

    adding delete button and return to home page

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -2,6 +2,8 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const AllCampusesView = (props) => {
+  const {deleteCampus} = props;
+
   if (!props.allCampuses.length) {
     return (
     <div>
@@ -21,11 +23,14 @@ const AllCampusesView = (props) => {
             <h1>{campus.name}</h1>
           </Link>
           <p>{campus.description}</p>
+          <button onClick={() => deleteCampus(campus.id)}>Delete</button>
         </div>
       ))}
-      <Link to={`newcampus`}>
+      <Link to={`/newcampus`}>
         <button>Add New Campus</button>
       </Link>
+      <br></br>
+      <Link to={`/`}>Home</Link>
     </div>
   );
 };
