3fa0442d073c26a5065c6beca5f95ea51b7517b5
Jane <fjane1210@gmail.com>
2021-12-18 13:39:06 -0500

    informative message if student is not enrolled at a campus

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -5,9 +5,12 @@ const StudentView = (props) => {
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <Link to={`/campus/${student.campus.id}`}>
+      {student.campus === null 
+      ? <h3>Not enrolled in a campus!</h3>
+      : <Link to={`/campus/${student.campus.id}`}>
         <h3>{student.campus.name}</h3>
       </Link>
+      }
       <h3>GPA: {student.gpa}</h3>
       <h3>Email: {student.email}</h3>
       <h3>imageURL: {student.imageURL}</h3>
